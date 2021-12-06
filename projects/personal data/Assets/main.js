be08038d57a867ba4e0f442f82e115497f210acb
amymeowzers <91495518+amymeowzers@users.noreply.github.com>
2021-12-06 13:59:59 -0500

    12/6

---
+++ projects/personal data/Assets/main.js
@@ -0,0 +1,41 @@
+    window.sr = ScrollReveal({reset:true});
+
+    sr.reveal('.animate-right', {
+        origin:'right',
+        duration:1500,
+        distance:'20rem',
+        delay: 300
+
+    })
+
+    sr.reveal('.animate-left', {
+        origin:'left',
+        duration:1500,
+        distance:'20rem',
+        delay: 300
+
+    })
+
+     sr.reveal('.animate-bottom', {
+        origin:'bottom',
+        duration:1500,
+        distance:'20rem',
+        delay: 300
+
+    })
+
+     sr.reveal('.animate-top', {
+        origin:'top',
+        duration:1500,
+        distance:'20rem',
+        delay: 300
+
+    })
+
+    ScrollReveal().reveal('.headline',{
+        delay: 300 });
+
+    ScrollReveal().reveal('.headline2', {
+        delay: 800 
+    });
+
